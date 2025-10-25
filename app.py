9e015a3641d6fa1a2f434903cee3f79a04b3ce88
minakshi-negi <negiminakshi635@gmail.com>
2025-10-25 12:40:59 +0530

    Add homepage route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps World!"
+    return "Hello, DevOps World from minaksi negi!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
